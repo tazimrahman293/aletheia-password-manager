b4dba087b1fc094c399a260942e73b2dd709a615
Tyrel Kostyk (tck290) <tck290@mail.usask.ca>
2022-03-08 13:27:08 -0600

    Updated Makefile to include test suite; implemented basic test suite

---
+++ test/include/test.h
@@ -0,0 +1,9 @@
+// test.h
+// Contains common includes and definitions for testing the Aletheia program.
+// Authors: Tyrel Kostyk
+
+
+#include <stdio.h>
+
+#define TEST_SUCCESS	(0)
+#define TEST_FAILURE	(-1)
