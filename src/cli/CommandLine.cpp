fad8c3e428a3d9e2c40b61949a8f28e83b9e636a
Tazim <tazimtrahman@gmail.com>
2022-03-14 21:13:52 -0600

    Added infinite while loop to CommandHandler and changed main to used InputParser.h implementation

--- src/cli/CommandLine.cpp
+++ src/cli/CommandLine.cpp
@@ -23,51 +23,52 @@
 
 
 void CommandLine::HandleCommand() {
+    while (true) {
+        if (InputExists("login") &&
+            (UserInputs[1] == "login")) {  // Login command
+            // jeremy: I foresee a potential problem here with this, what if the user enters "aletheia garbage login" should we not do something about the garbage input and tell the user about it? int userId = std::stoi(); std::vector<Account> userAccount = Storage::GetAllAccountsByUserID(userId);
 
-    if (InputExists("login") && (UserInputs[1] == "login")){ // Login command
-        // jeremy: I foresee a potential problem here with this, what if the user enters "aletheia garbage login"
-        // should we not do something about the garbage input and tell the user about it?
-        // int userId = std::stoi();
-        // std::vector<Account> userAccount = Storage::GetAllAccountsByUserID(userId);
+            // jeremy: The pk attribute belongs to one individual Account, not a vector of them if (userAccount->pk == InputArg[2]){
+            //     UpdateOutput("Login successful from user");
+            // }
 
-        // jeremy: The pk attribute belongs to one individual Account, not a vector of them
-        // if (userAccount->pk == InputArg[2]){
-        //     UpdateOutput("Login successful from user");
-        // }
+            // Storing user inputs
+            std::string FirstName = UserInputs[2];
+            std::string LastName = UserInputs[3];
+            // std::string Input_password = UserInputs[4];
 
-        // Storing user inputs
-        std::string FirstName = UserInputs[2];
-        std::string LastName = UserInputs[3];
-        //std::string Input_password = UserInputs[4];
+            std::vector<User> ExtractedUserVector =
+                UserDatabase->GetAllUsers();  // Grab database of all users
+            // Don't know if that's the correct way of grabbing the storage, but it's the implementation for now
 
-        std::vector<User> ExtractedUserVector = UserDatabase->GetAllUsers(); // Grab database of all users
-        // Don't know if that's the correct way of grabbing the storage, but it's the implementation for now
-
-        for (User user: ExtractedUserVector) {
-            if ((user.firstName == FirstName) && (user.lastName == LastName)) {  // If password exists and matches user input, login is successful
-                // std::cout << "Login successful. Welcome " << *Database_ID->firstName <<" " << *Database_ID->lastName << std::endl;
-                std::cout << "Access to structure successful" << std::endl;
-                break;
-            } else {
-                std::cout << "Invalid login input" << std::endl;
+            for (User user : ExtractedUserVector) {
+                if ((user.firstName == FirstName) &&
+                    (user.lastName ==
+                     LastName)) {  // If password exists and matches user input, login is successful
+                    // std::cout << "Login successful. Welcome " << *Database_ID->firstName <<" " << *Database_ID->lastName << std::endl;
+                    std::cout << "Access to structure successful" << std::endl;
+                    break;
+                } else {
+                    std::cout << "Invalid login input" << std::endl;
+                }
             }
+
+            std::cout << "Login command finished being executed" << std::endl;
+
+        }
+        // jeremy: should this be else if? probably don't want to both log in and create a new account and edit an account in the same command
+        else if (InputExists("--new-account") &&
+                 (UserInputs[1] == "--new-account")) {  // Create account
+
+            std::cout << "Registration command working" << std::endl;
         }
 
-        std::cout << "Login command finished being executed" << std::endl;
-
+        // jeremy: same comment as above
+        else if (InputExists("--edit-account") &&
+                 (UserInputs[1] == "--edit-account")) {
+            std::cout << "Updating account command working" << std::endl;
+        }
     }
-    // jeremy: should this be else if? probably don't want to both log in and create a new account and edit an account
-    // in the same command
-    else if (InputExists("--new-account") && (UserInputs[1] == "--new-account")){ // Create account
-
-        std::cout << "Registration command working" << std::endl;
-    }
-
-    // jeremy: same comment as above
-    else if (InputExists("--edit-account") && (UserInputs[1] == "--edit-account")){
-        std::cout << "Updating account command working" << std::endl;
-    }
-    std::cout << "Command Handler was ran" << std::endl;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,67 +9,73 @@
 #include <Storage.h>
 
 #include "network/HTTPServer.h"
+#include "cli/CommandLine.h"
+#include "cli/InputParser.h"
 
-
-/**
- * Gets flags/options/positional arguments from the command line using argc and argv)
- */
-class InputParser {
-
-    // All individual (space-delimited) tokens from the command-line input
-    std::vector<std::string> tokens;
-
-public:
-
-    InputParser(int& argc, char **argv) {
-        for (int i = 1; i < argc; ++i)
-            tokens.emplace_back(std::string(argv[i]));
-    }
-
-    [[nodiscard]]
-    bool HasOption(const std::string& option) const {
-        return std::find(tokens.begin(), tokens.end(), option) != tokens.end();
-    }
-
-    [[nodiscard]]
-    const std::string& GetOptionValue(const std::string& option) const {
-        std::vector<std::string>::const_iterator it;
-        it = std::find(tokens.begin(), tokens.end(), option);
-        if (it != tokens.end() && ++it != tokens.end())
-            return *it;
-        static const std::string empty_str;
-        return empty_str;
-    }
-};
+///**
+// * Gets flags/options/positional arguments from the command line using argc and argv)
+// */
+//class InputParser {
+//
+//    // All individual (space-delimited) tokens from the command-line input
+//    std::vector<std::string> tokens;
+//
+//public:
+//
+//    InputParser(int& argc, char **argv) {
+//        for (int i = 1; i < argc; ++i)
+//            tokens.emplace_back(std::string(argv[i]));
+//    }
+//
+//    [[nodiscard]]
+//    bool HasOption(const std::string& option) const {
+//        return std::find(tokens.begin(), tokens.end(), option) != tokens.end();
+//    }
+//
+//    [[nodiscard]]
+//    const std::string& GetOptionValue(const std::string& option) const {
+//        std::vector<std::string>::const_iterator it;
+//        it = std::find(tokens.begin(), tokens.end(), option);
+//        if (it != tokens.end() && ++it != tokens.end())
+//            return *it;
+//        static const std::string empty_str;
+//        return empty_str;
+//    }
+//};
 
 /**
  * Manages passwords
  */
 int main(int argc, char *argv[])
 {
-    InputParser cmdParser(argc, argv);
+    InputParser cmdParser(argc, argv, nullptr); // nullptr since InputParser takes database input
 
     // Set database filename (either using -d value or default to db.sqlite3)
     std::string dbFilename;
-    if (cmdParser.HasOption("-d"))
-        dbFilename = cmdParser.GetOptionValue("-d");
+    if (cmdParser.InputExists("-d"))
+        dbFilename = cmdParser.GetOption("-d");
     else
         dbFilename = "db.sqlite3";
     // Instantiate storage with filename
     Storage database(dbFilename);
 
+    // Start in CLI if -CLI is given
+    if (cmdParser.InputExists("-CLI")) {
+        CommandLine CLI(argc, argv, &database);
+        CLI.HandleCommand();
+    }
     // Start in Server mode if -S given (no stdin, request come through HTTP)
-    if (cmdParser.HasOption("-S")) {
+    if (cmdParser.InputExists("-S")) {
 
         // Host address/name (from -H value, defaults to 0.0.0.0)
         std::string hostAddress = "0.0.0.0";
-        if (cmdParser.HasOption("-H"))
-            hostAddress = cmdParser.GetOptionValue("-H");
+        if (cmdParser.InputExists("-H"))
+            hostAddress = cmdParser.GetOption("-H");
 
         // Host port (from -P value, defaults to 8089)
         int hostPort = 8089;
-        if (cmdParser.HasOption("-P"))
-            hostPort = std::stoi(cmdParser.GetOptionValue("-P"));
+        if (cmdParser.InputExists("-P"))
+            hostPort = std::stoi(cmdParser.GetOption("-P"));
 
         // Spin up server instance and run listen loop
         HTTPServer server(hostAddress, hostPort);
