067f2235c73f3ae0d46f6670bb3384c2797b9fd5
akg650 <akg50@mail.usask.ca>
2022-03-17 02:02:33 -0600

    Added react-helmet, to add titles to all pages

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import HomeSection from '../components/HomeSection'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import DataSection from '../components/DataSection'
 import { dataAbout, dataFeautures } from '../components/DataSection/Data'
 import Team from '../components/Team'
+import { Helmet } from "react-helmet";
 
 /**
  * This is the main page which uses all the components and renders the App
@@ -12,20 +13,24 @@ import Team from '../components/Team'
  */
 const Home = () => {
   // debugger
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
-      <Sidebar isOpen={isOpen} toggle={toggle}/>
-      <Navbar toggle={toggle}/>
-      <HomeSection/>
-      <DataSection {...dataAbout}/>
-      <DataSection {...dataFeautures}/>
-      <Team/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home</title>
+      </Helmet>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HomeSection />
+      <DataSection {...dataAbout} />
+      <DataSection {...dataFeautures} />
+      <Team />
     </>
   );
 }
