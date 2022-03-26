d4ff898de01d2271684b01a64d65ba5e48617269
tyrelkostyk <tck290@mail.usask.ca>
2022-03-26 17:22:20 -0600

    extended the test cases for feature 5-1 (RNG); all tests passing

--- test/random/test_5-1.cpp
+++ test/random/test_5-1.cpp
@@ -19,30 +19,74 @@ TEST_CASE("random-constructor") {
 
 TEST_CASE("random-bounds") {
     RandomNumberGenerator *rng = new RandomNumberGenerator();
-    int minBound, maxBound;
-    int newValue;
+    int minBound = 0, maxBound = 0;
+    int newValue = 0, oldValue = 0;
+	int matchCounter = 0, matchMax = 0;
 
-    SUBCASE("random-bounds-0-10") {
-        minBound = 0; maxBound = 10;
-    }
+	SUBCASE("random-valid-bounds") {
+		SUBCASE("random-valid-bounds-0-10") {
+	        minBound = 0; maxBound = 10;
+	    }
 
-    SUBCASE("random-bounds-1-1") {
-        minBound = maxBound = 1;
-    }
+	    SUBCASE("random-valid-bounds-1-1") {
+	        minBound = maxBound = 1;
+	    }
 
-    SUBCASE("random-bounds-n10-0") {
-        minBound = -10; maxBound = 0;
-    }
+		SUBCASE("random-valid-bounds-1-1") {
+	        minBound = 22;
+			maxBound = 99;
+	    }
 
-    SUBCASE("random-bounds-10-0") {
-        minBound = 10; maxBound = 0;
-    }
+		// run through entire range twice
+		int testCount = 2 * (maxBound - minBound);
+		// don't allow more than half of the range to be duplicates
+		oldValue = 0;
+		matchCounter = 0;
+		matchMax = testCount / 2;
+		for (int i = 0; i < testCount; i++) {
+			// get new value
+			newValue = rng->GetNew(minBound, maxBound);
+			// check new value is above (or equal to) minimum bound
+			CHECK_GE(newValue, minBound);
+			// check new value is below (or equal to) maximum bound
+			CHECK_LE(newValue, maxBound);
+			// increment counter if a match is detected
+			if (oldValue == newValue)
+				matchCounter++;
+			// check that too many matches have not occured
+			CHECK_LE(matchCounter, matchMax);
+			// store new value
+			oldValue = newValue;
+		}
+	}
 
-    for (int i = 0; i < 2 * (maxBound - minBound + 1); i++) {
-        newValue = rng->GetNew(minBound, maxBound);
-        CHECK_GE(newValue, minBound);
-        CHECK_LE(newValue, maxBound);
-    }
+	SUBCASE("random-invalid-bounds") {
+		SUBCASE("random-invalid-bounds-n10-0") {
+	        minBound = -10; maxBound = 0;
+	    }
+
+	    SUBCASE("random-invalid-bounds-10-0") {
+	        minBound = 10; maxBound = 0;
+	    }
+
+		SUBCASE("random-invalid-bounds-1-0") {
+			minBound = 1; maxBound = 0;
+		}
+
+		SUBCASE("random-invalid-bounds-n1-100") {
+			minBound = -1; maxBound = 100;
+		}
+
+		SUBCASE("random-invalid-bounds-100-99") {
+			minBound = 100; maxBound = 99;
+		}
+
+		// get new value
+		newValue = rng->GetNew(minBound, maxBound);
+
+		// confirm that error value (0) is returned
+		CHECK_EQ(newValue, 0);
+	}
 }
 
 TEST_SUITE_END();
