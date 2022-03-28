244cd2a639dace7af757894beb94d6b8b07b58d8
Jeremy Rempel <jeremy.rempel@usask.ca>
2022-03-28 10:16:45 -0600

    prepare to write unit tests for cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 // main.cpp
 // Contains the main entrypoint for the TBD password manager project.
-// Authors: <insert here>
+// Authors: Tyrel Kostyk, Tazim Rahman, Jeremy Rempel
 
-#include <algorithm>
 #include <string>
-#include <vector>
 
 #include <Storage.h>
 #include <hydrogen.h>
@@ -13,36 +11,6 @@
 #include "cli/CommandLine.h"
 #include "cli/InputParser.h"
 
-///**
-// * Gets flags/options/positional arguments from the command line using argc and argv)
-// */
-//class InputParser {
-//
-//    // All individual (space-delimited) tokens from the command-line input
-//    std::vector<std::string> tokens;
-//
-//public:
-//
-//    InputParser(int& argc, char **argv) {
-//        for (int i = 1; i < argc; ++i)
-//            tokens.emplace_back(std::string(argv[i]));
-//    }
-//
-//    [[nodiscard]]
-//    bool HasOption(const std::string& option) const {
-//        return std::find(tokens.begin(), tokens.end(), option) != tokens.end();
-//    }
-//
-//    [[nodiscard]]
-//    const std::string& GetOptionValue(const std::string& option) const {
-//        std::vector<std::string>::const_iterator it;
-//        it = std::find(tokens.begin(), tokens.end(), option);
-//        if (it != tokens.end() && ++it != tokens.end())
-//            return *it;
-//        static const std::string empty_str;
-//        return empty_str;
-//    }
-//};
 
 /**
  * Manages passwords
--- test/cli/test_1-1.cpp
+++ test/cli/test_1-1.cpp
@@ -1,12 +1,16 @@
 //
 // Created by Tazim Rahman on 2022-03-26.
 //
+
+
 #include "doctest.h"
 
-#include "include/cli/CommandLine.h"
+#include "cli/CommandLine.h"
 
 TEST_SUITE_BEGIN("cli");
 
 TEST_CASE("cli-constructor") {
-    Commandline cli = new Commandline()
+    Commandline cli = new Commandline();
 }
+
+TEST_SUITE_END();
