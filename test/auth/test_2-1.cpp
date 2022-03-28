c34801f1b94cc6204144ee206e94c1af51611030
tyrelkostyk <tck290@mail.usask.ca>
2022-03-28 11:04:32 -0600

    added test cases for feature 2-1; all tests passing

---
+++ test/auth/test_2-1.cpp
@@ -0,0 +1,69 @@
+// test_2-1.cpp
+// Contains the function definitions for testing User Story 2-1.
+// Authors: Tyrel Kostyk
+
+#include "auth/Authenticator.h"
+
+#include "doctest.h"
+
+TEST_SUITE_BEGIN("auth");
+
+TEST_CASE("auth-construct") {
+	Authenticator *auth = nullptr;
+    CHECK_EQ(auth, nullptr);
+
+    auth = new Authenticator();
+    REQUIRE_NE(auth, nullptr);
+}
+
+
+TEST_CASE("auth-hash-and-verify") {
+	Authenticator *auth = new Authenticator();
+	const int hashSize = 128;
+	const int sampleSize = 10;
+
+	std::string password = "";
+	std::string passwordAttempt = "";
+	std::vector<uint8_t> hash[sampleSize];
+	bool valid = false;
+
+	SUBCASE("auth-hash-and-verify-password") {
+		password = "password";
+	}
+
+	SUBCASE("auth-hash-and-verify-password-special") {
+		password = "pa$$word1234567890______~`!@#$%^&*()_-+={[}]|:;,.?";
+	}
+
+	SUBCASE("auth-hash-and-verify-password-short") {
+		password = "p";
+	}
+
+	SUBCASE("auth-hash-and-verify-password-long") {
+		password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	}
+
+	// create many unique hashes for the same password
+	for (int i = 0; i < sampleSize; i++) {
+		hash[i] = auth->Hash(password);
+
+		// check that hash is correct size
+		CHECK_EQ(hash[i].size(), hashSize);
+
+		// check that all hashes are unique
+		for (int j = 1; i-j >= 0; j++) {
+			CHECK_NE(hash[i], hash[i-j]);
+		}
+
+		// check that new hash still validates password
+		valid = auth->Verify(hash[i], password);
+		CHECK(valid);
+
+		// check that an invalid password does not get validated
+		passwordAttempt = "__";
+		valid = auth->Verify(hash[i], passwordAttempt);
+		CHECK(!valid);
+	}
+}
+
+TEST_SUITE_END();
